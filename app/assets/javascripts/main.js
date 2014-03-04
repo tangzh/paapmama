c498c105453d9cbfb0a256a57f185d0229f7123d
Tang Zhang <zhangtang2013@gmail.com>
2014-03-04 01:01:42 -0800

    add images

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,12 +7,15 @@
       height: 210
     },
     windowHeight: $(window).height(),
-    windowWidth: $(window).width()
+    windowWidth: $(window).width(),
+    resource: ["https://mail.google.com/mail/ca/u/0/?ui=2&ik=a11958858d&view=att&th=1448bf558e17803f&attid=0.9&disp=safe&zw", "https://mail.google.com/mail/ca/u/0/?ui=2&ik=a11958858d&view=att&th=1448bf558e17803f&attid=0.5&disp=safe&zw", "https://mail.google.com/mail/ca/u/0/?ui=2&ik=a11958858d&view=att&th=1448bf558e17803f&attid=0.7&disp=safe&zw","https://mail.google.com/mail/ca/u/0/?ui=2&ik=a11958858d&view=att&th=1448bf558e17803f&attid=0.3&disp=safe&zw","https://mail.google.com/mail/ca/u/0/?ui=2&ik=a11958858d&view=att&th=1448bf558e17803f&attid=0.1&disp=safe&zw"
+    ]
   };
 
-  function Photo( pIndex ) {
+  function Photo( pIndex , resource) {
     this.pIndex = pIndex;
     this.size = this.calculateSize();
+    this.resource =  resource;
 
     // Placed after size is calculated
     this.position = this.calculatePosition();
@@ -53,26 +56,31 @@
   Photo.prototype.render = function() {
     var $container = $('main'), 
         _this = this,
+        url = 'url(' + _this.resource + ')',
         $imageContainer = $('<div></div>')
           .addClass('my-photo')
           .css({
             left: _this.position.left,
             top: _this.position.top,
             width: _this.size.width,
-            height: _this.size.height
+            height: _this.size.height,
+            backgroundImage: url
           });
 
     $container.append($imageContainer);
   };
 
-  function init() {
-  	var settings = {
-  		count: 3
-  	};
+  
 
-    for ( var i = 0; i < settings.count; i++ ) {
-      var photo = new Photo( i + 2 );
+  function init() {
+
+    for ( var i = 0; i < 3 ; i++ ) {
+      var photo = new Photo( i + 2 , settings.resource[i]);
       photo.render();
     }
   };
+
+
+
+
 //})()
